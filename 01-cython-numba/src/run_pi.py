1007b71b02a9a4bf94020429e73b8b8bb3b10f00
Kamil Jarosz <kjarosh256@gmail.com>
2020-03-26 02:22:21 +0100

    Fix dataframes

--- 01-cython-numba/src/run_pi.py
+++ 01-cython-numba/src/run_pi.py
@@ -6,6 +6,7 @@ import pandas
 import pyximport
 import numba
 from matplotlib import pyplot as plt
+from pandas import DataFrame
 
 pyximport.install(language_level=3)
 
@@ -81,7 +82,7 @@ pandas.set_option('display.width', 1000)
 
 def test():
     print("size:", size)
-    results = pandas.DataFrame(columns=['type', 'name', 'time'])
+    results = DataFrame(columns=['type', 'name', 'time'])
     for td in testing_data:
         if 'filter' in td and not td['filter'](size):
             time = None
@@ -100,50 +101,52 @@ def test():
 
 
 def test_sizes(sizes, plot):
-    results = []
-    for i in sizes:
-        global size
-        size = i
-        file_name = "result-{}.dat".format(i)
+    file_name = "result-sizes.dat"
 
-        if plot:
-            r = pandas.read_csv(file_name, sep="\t")
-        else:
+    if plot:
+        results = pandas.read_csv(file_name, sep="\t")
+    else:
+        results = DataFrame(columns=['type', 'name', 'time', 'size'])
+        for i in sizes:
+            global size
+            size = i
             r = test()
-            r.to_csv(file_name, sep="\t", index_label=False)
+            r['size'] = i
+            results = pandas.concat([results, r], ignore_index=True)
 
-        results.append(r)
+        results.to_csv(file_name, sep="\t", index=False)
 
-    plot_results(results, sizes, 'Computation time of Pi number depending on points number', 'points', 'time [s]')
+    plot_results(results, 'size', 'Computation time of Pi number depending on points number', 'size', 'time [s]')
 
 
-def test_sizes_threads(sizes, threads, plot):
-    for i in sizes:
-        global size
-        size = i
-        results = []
+def test_threads(threads, plot):
+    global size
+    size = 60000
+    file_name = "result-threads.dat"
+
+    if plot:
+        results = pandas.read_csv(file_name, sep="\t")
+    else:
+        results = DataFrame(columns=['type', 'name', 'time', 'threads'])
         for j in threads:
-            print("threads: ", j)
+            print("threads:", j)
             global processes
             processes = j
             numba.set_num_threads(min(j, numba.config.NUMBA_NUM_THREADS))
-            file_name = "result-{}-{}.dat".format(i, j)
+            r = test()
+            r['threads'] = j
+            results = pandas.concat([results, r], ignore_index=True)
 
-            if plot:
-                r = pandas.read_csv(file_name, sep="\t")
-            else:
-                r = test()
-                r.to_csv(file_name, sep="\t", index_label=False)
-            results.append(r)
-        plt_title = "Computation time of Pi number depending on threads number for {} points".format(i)
-        plot_results(results, threads, plt_title, 'threads', 'time [s]')
+        results.to_csv(file_name, sep="\t", index=False)
+
+    plt_title = "Computation time of Pi number depending on threads number for {} points".format(size)
+    plot_results(results, 'threads', plt_title, 'threads', 'time [s]')
 
 
 def plot_results(results, index, title, xlabel, ylabel):
-    columns = ['{} {}'.format(value[0], value[1]) for value in results[0].values]
-    data = [[value[2] for value in result.values] for result in results]
-    df = pandas.DataFrame(data, index=index, columns=columns)
-    df.plot(colormap='jet', marker='.', markersize=10, title=title)
+    results['kind'] = results['type'].astype(str) + ' ' + results['name']
+    results.set_index(index, inplace=True)
+    results.groupby('kind')['time'].plot(legend=True, title=title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.yscale('log')
@@ -152,10 +155,10 @@ def plot_results(results, index, title, xlabel, ylabel):
 
 def main():
     plot = len(sys.argv) >= 2 and sys.argv[1] == 'plot'
-    sizes = [100000, 200000, 300000, 400000, 500000, 600000]
-    threads = [1, 2, 4, 8, 16]
+    sizes = [10000, 20000, 30000, 40000, 50000, 60000]
+    threads = [1, 2, 3, 4, 5, 6, 7, 8]
     test_sizes(sizes, plot)
-    test_sizes_threads(sizes, threads, plot)
+    test_threads(threads, plot)
 
 
 if __name__ == '__main__':
